8f59486674668bd1901aaeeb9276ea8145ca334b
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-09-10 17:19:18 -0600

    Added limited unit tests.

---
+++ test_calc_acs_db.py
@@ -0,0 +1,57 @@
+"""Unit tests for the calc_acs_dp all common subsequences mini-module.
+
+@author: A. Samuel Pottinger
+@license: MIT
+"""
+
+import unittest
+
+import calc_acs_dp
+
+ALL_SHARED_1 = 'est'
+ALL_SHARED_2 = 'est time'
+
+PARTIALLY_SHARED_1 = 'qwer'
+PARTIALLY_SHARED_2 = 'qewr'
+
+NONE_SHARED_1 = 'mine'
+NONE_SHARED_2 = 'yours'
+
+
+class TestCalcACS(unittest.TestCase):
+    """Test suite for the calc_acs_dp mini-module."""
+
+    def test_count_common_subsequences(self):
+        """Test counting the number of subsequences without listing them."""
+        no_shared = calc_acs_dp.count_common_subsequences(NONE_SHARED_1,
+            NONE_SHARED_2)
+        self.assertEqual(no_shared, 1)
+
+        partially_shared = calc_acs_dp.count_common_subsequences(
+            PARTIALLY_SHARED_1, PARTIALLY_SHARED_2)
+        self.assertEqual(partially_shared, 12)
+
+        all_shared = calc_acs_dp.count_common_subsequences(ALL_SHARED_1,
+            ALL_SHARED_2)
+        self.assertEqual(all_shared, 8)
+
+    def test_find_common_subsequences(self):
+        """Test getting a list of subseqeunces between two sequences."""
+        no_shared = calc_acs_dp.find_common_subsequences(NONE_SHARED_1,
+            NONE_SHARED_2)
+        self.assertEqual(no_shared, set(['']))
+
+        partially_shared = calc_acs_dp.find_common_subsequences(
+            PARTIALLY_SHARED_1, PARTIALLY_SHARED_2)
+        components = ['', 'qer', 'wr', 'qwr', 'er', 'qr', 'e', 'qw', 'q', 'r',
+            'qe', 'w']
+        self.assertEqual(partially_shared, set(components))
+
+        all_shared = calc_acs_dp.find_common_subsequences(ALL_SHARED_1,
+            ALL_SHARED_2)
+        components = ['', 'e', 's', 't', 'es', 'st', 'et', 'est']
+        self.assertEqual(all_shared, set(components))
+
+
+if __name__ == '__main__':
+    unittest.main()
