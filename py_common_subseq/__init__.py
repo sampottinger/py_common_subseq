6a71a04ca24253a7c28994cc3cfe73c15119d43d
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-09-10 17:32:54 -0600

    Reorganized to python standard file layout.

---
+++ py_common_subseq/__init__.py
@@ -0,0 +1,14 @@
+"""Micro-library implementating logic to find all common subsequences.
+
+Implementation of the calACS-DP (all common subsequences - dynamic programming)
+algorithm as presented in Hui Wang's "All common subsequences."
+IJCAI 2007:635-640 (ACM). This Micro-library also introduces a variation which
+can produce a list all of those common subsequences. Additionally, both the
+classic implementation as well as the variation offer some space optimizations
+similar to those found in longest common subsequence imeplementations.
+
+@author: A. Samuel Pottinger (http://gleap.org)
+@license: MIT
+"""
+
+from py_common_subseq import *
