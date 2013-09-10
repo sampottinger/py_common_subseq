101808f7f93a9687fb1d48589af81a72044b79ff
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-09-10 17:33:42 -0600

    Fixed include name for unit tests.

--- py_common_subseq/test_py_common_subseq.py
+++ py_common_subseq/test_py_common_subseq.py
@@ -6,7 +6,7 @@
 
 import unittest
 
-import calc_acs_dp
+import py_common_subseq
 
 ALL_SHARED_1 = 'est'
 ALL_SHARED_2 = 'est time'
@@ -19,35 +19,35 @@ NONE_SHARED_2 = 'yours'
 
 
 class TestCalcACS(unittest.TestCase):
-    """Test suite for the calc_acs_dp mini-module."""
+    """Test suite for the py_common_subseq mini-module."""
 
     def test_count_common_subsequences(self):
         """Test counting the number of subsequences without listing them."""
-        no_shared = calc_acs_dp.count_common_subsequences(NONE_SHARED_1,
+        no_shared = py_common_subseq.count_common_subsequences(NONE_SHARED_1,
             NONE_SHARED_2)
         self.assertEqual(no_shared, 1)
 
-        partially_shared = calc_acs_dp.count_common_subsequences(
+        partially_shared = py_common_subseq.count_common_subsequences(
             PARTIALLY_SHARED_1, PARTIALLY_SHARED_2)
         self.assertEqual(partially_shared, 12)
 
-        all_shared = calc_acs_dp.count_common_subsequences(ALL_SHARED_1,
+        all_shared = py_common_subseq.count_common_subsequences(ALL_SHARED_1,
             ALL_SHARED_2)
         self.assertEqual(all_shared, 8)
 
     def test_find_common_subsequences(self):
         """Test getting a list of subseqeunces between two sequences."""
-        no_shared = calc_acs_dp.find_common_subsequences(NONE_SHARED_1,
+        no_shared = py_common_subseq.find_common_subsequences(NONE_SHARED_1,
             NONE_SHARED_2)
         self.assertEqual(no_shared, set(['']))
 
-        partially_shared = calc_acs_dp.find_common_subsequences(
+        partially_shared = py_common_subseq.find_common_subsequences(
             PARTIALLY_SHARED_1, PARTIALLY_SHARED_2)
         components = ['', 'qer', 'wr', 'qwr', 'er', 'qr', 'e', 'qw', 'q', 'r',
             'qe', 'w']
         self.assertEqual(partially_shared, set(components))
 
-        all_shared = calc_acs_dp.find_common_subsequences(ALL_SHARED_1,
+        all_shared = py_common_subseq.find_common_subsequences(ALL_SHARED_1,
             ALL_SHARED_2)
         components = ['', 'e', 's', 't', 'es', 'st', 'et', 'est']
         self.assertEqual(all_shared, set(components))
